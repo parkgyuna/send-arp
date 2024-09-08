913b42c8c2ddaa0c353a74256835fb4c52397e98
gilgil <gilgil@gilgil.net>
2024-09-08 15:45:19 +0900

    Update variable name and symbols

--- src/main.cpp
+++ src/main.cpp
@@ -18,21 +18,21 @@ void usage() {
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
 		usage();
-		return -1;
+		return EXIT_FAILURE;
 	}
 
 	char* dev = argv[1];
 	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* handle = pcap_open_live(dev, 0, 0, 0, errbuf);
-	if (handle == nullptr) {
+	pcap_t* pcap = pcap_open_live(dev, 0, 0, 0, errbuf);
+	if (pcap == nullptr) {
 		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
-		return -1;
+		return EXIT_FAILURE;
 	}
 
 	EthArpPacket packet;
 
-	packet.eth_.dmac_ = Mac("00:00:00:00:00:00");
-	packet.eth_.smac_ = Mac("00:00:00:00:00:00");
+	packet.eth_.dmac_ = Mac("FF:FF:FF:FF:FF:FF");
+	packet.eth_.smac_ = Mac("b4:2e:99:ea:97:de");
 	packet.eth_.type_ = htons(EthHdr::Arp);
 
 	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
@@ -40,15 +40,15 @@ int main(int argc, char* argv[]) {
 	packet.arp_.hln_ = Mac::SIZE;
 	packet.arp_.pln_ = Ip::SIZE;
 	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.sip_ = htonl(Ip("0.0.0.0"));
+	packet.arp_.smac_ = Mac("00:11:22:33:44:55");
+	packet.arp_.sip_ = htonl(Ip("10.1.1.1"));
 	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.tip_ = htonl(Ip("0.0.0.0"));
+	packet.arp_.tip_ = htonl(Ip("10.1.1.1"));
 
-	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+	int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 	if (res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
 	}
 
-	pcap_close(handle);
+	pcap_close(pcap);
 }
